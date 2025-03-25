59f39c3336edf7e2791fb7721aff575533274b6f
Casey Tomchuk <Caseytomchuk@gmail.com>
2025-03-25 13:52:44 -0400

    Task 3 - Fetch Products with async/await

--- cc_16.js
+++ cc_16.js
@@ -2,7 +2,7 @@
 
 // This function fetches the API, then if the response is ok (not an error, such as 404) it will return the json data.
 // From there we want to iterate through every item and log the names. We will catch any errors and display them.
-function fetchProductsThen() {
+/*function fetchProductsThen() {
     fetch(`https://www.course-api.com/javascript-store-products`)
     .then(response => {
 // If response is not ok, throw an error
@@ -16,4 +16,40 @@ function fetchProductsThen() {
     .catch(error => console.error(error)); // catch the thrown error if necessary
 }; 
 
-fetchProductsThen(); 
+fetchProductsThen(); */
+
+// Task 3: Fetch products with async/ wait 
+
+// Text
+productContainer = document.getElementById("product-container");
+
+async function fetchProductsAsync() {
+    try {
+        const response = await fetch (`https://www.course-api.com/javascript-store-products`)
+
+        if(!response.ok){
+            throw new Error("Response was not ok");
+        }
+// iterating over each item to run displayProducts for each
+        const data = await response.json();
+        data.forEach(item => displayProducts(item)) // (item) may not be correct
+        data.forEach(item => console.log(item)); // for testing purposes
+    }
+
+    catch {
+        handleError(Error) // this will change on the next commit
+    }
+
+}
+
+function displayProducts(products) {
+
+}
+
+function handleError(error) {
+// console.error(error) (maybe??)
+}
+
+fetchProductsAsync();
+
+//data.forEach(item => productContainer.appendChild(item))
