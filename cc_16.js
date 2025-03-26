0acaf9f7d103d9083ec52fe176cb418b6a4f6d0e
Casey Tomchuk <Caseytomchuk@gmail.com>
2025-03-26 11:45:08 -0400

    Task 4 - Display products in the DOM with name, price, and image

--- cc_16.js
+++ cc_16.js
@@ -1,27 +1,23 @@
 // Task 2: Fetch Products with .then()
 
-// This function fetches the API, then if the response is ok (not an error, such as 404) it will return the json data.
-// From there we want to iterate through every item and log the names. We will catch any errors and display them.
-/*function fetchProductsThen() {
+// fetches api, then if the response is ok (not an error, such as 404) it will return the json data. then it will iterate through every item and log the names.
+function fetchProductsThen() {
     fetch(`https://www.course-api.com/javascript-store-products`)
     .then(response => {
-// If response is not ok, throw an error
+    // If response is not ok, throw an error
         if (!response.ok){
             throw new Error("Whoops", response.statusText)
         }
         return response.json() // otherwise return the json data
     })
-// After we get the json data, we want to log each item using a forEach statement. I found that I needed to access .fields.name by using ctrl + shift + J on my html tab
+    // After we get the json data, we want to log each item using a forEach statement. I found that I needed to access .fields.name because I used ctrl + shift + J on my html tab
     .then(names => names.forEach(itemName => console.log(itemName.fields.name))) 
     .catch(error => console.error(error)); // catch the thrown error if necessary
 }; 
 
-fetchProductsThen(); */
+fetchProductsThen(); 
 
 // Task 3: Fetch products with async/ wait 
-
-// Text
-productContainer = document.getElementById("product-container");
 
 async function fetchProductsAsync() {
     try {
@@ -30,20 +26,32 @@ async function fetchProductsAsync() {
         if(!response.ok){
             throw new Error("Response was not ok");
         }
-// iterating over each item to run displayProducts for each
+
+    // Iterating over the first 5 items. I initially tried to do this with a for loop in displayProducts but it made it so that each item name printed 5 times.
         const data = await response.json();
-        data.forEach(item => displayProducts(item)) // (item) may not be correct
-        data.forEach(item => console.log(item)); // for testing purposes
+    //For the first 5 items, we are running displayProducts and taking name, price, and image as a parameter
+        data.slice(0,5).forEach(item => displayProducts(item.fields.name, item.fields.price, item.fields.image));
     }
 
     catch {
-        handleError(Error) // this will change on the next commit
+        handleError(error) // this will change on the next commit
     }
-
 }
 
-function displayProducts(products) {
+// Task 4:
 
+// Creating a new paragraph element and setting the text content of it to products and price, then appending it to the productContainer div. Doing something similar for the image
+function displayProducts(products, price, image) {
+
+    const productContainer = document.getElementById("product-container");
+    const productName = document.createElement("p");
+    productName.textContent = `${products}, ${price}`;
+
+    const productImage = document.createElement("img");
+    productImage.src = image[0].url // The image is an object contained in an array ("image"). So we have to access the url of correct item [0] to display it
+
+    productContainer.appendChild(productName);
+    productContainer.appendChild(productImage);
 }
 
 function handleError(error) {
@@ -51,5 +59,3 @@ function handleError(error) {
 }
 
 fetchProductsAsync();
-
-//data.forEach(item => productContainer.appendChild(item))
